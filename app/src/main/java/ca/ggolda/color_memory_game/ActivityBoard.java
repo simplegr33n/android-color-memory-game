d8a95c0234d365a89cb8670aecbde58cd906e56e
simplegr33n <gcgolda@gmail.com>
2016-12-07 13:14:16 -0800

    added random number generator

--- app/src/main/java/ca/ggolda/color_memory_game/ActivityBoard.java
+++ app/src/main/java/ca/ggolda/color_memory_game/ActivityBoard.java
@@ -10,11 +10,14 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Random;
 
 
 public class ActivityBoard extends AppCompatActivity {
 
     private String PatternString = "123414";
+    private int Min = 1;
+    private int Max = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +47,23 @@ public class ActivityBoard extends AppCompatActivity {
                     //Make background white on-pressed
                     gameBoard.setBackgroundColor(Color.parseColor("#FFFFFF"));
 
-                    // Add a value to the end of the Pattern String
-                    PatternString = PatternString + "3";
+                    // Add a Random value to the end of the Pattern String
+                    Random r = new Random();
+                    int i1 = r.nextInt(Max - Min + 1) + Min;
+                    PatternString = PatternString + i1;
                     patternString.setText(PatternString);
+
                 } else if (event.getAction() == MotionEvent.ACTION_UP && event.getAction() != MotionEvent.ACTION_CANCEL)// && flag==true)
                 {
                     // Another unnecessary Log
                     Log.d("Released", "Button released");
                     //Return background black on-released
                     gameBoard.setBackgroundColor(Color.parseColor("#000000"));
+                    
                 } else return false;
 
+
+                //Must return true here in order to pick up ACTION_UP
                 return true;
             }
         });
