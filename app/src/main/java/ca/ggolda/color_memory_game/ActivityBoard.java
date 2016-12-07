a5e6ff6d7b1f00b25fd201ed07186e78d0473b53
simplegr33n <gcgolda@gmail.com>
2016-12-07 14:14:23 -0800

    added Go! button and used handler to display sequence in a TextView

--- app/src/main/java/ca/ggolda/color_memory_game/ActivityBoard.java
+++ app/src/main/java/ca/ggolda/color_memory_game/ActivityBoard.java
@@ -3,6 +3,7 @@ package ca.ggolda.color_memory_game;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -13,9 +14,12 @@ import android.widget.TextView;
 import java.util.Random;
 
 
+
 public class ActivityBoard extends AppCompatActivity {
 
+    private Handler patternHandler;
     private String PatternString = "123414";
+    private int currentIndex = 0;
     private int Min = 1;
     private int Max = 4;
 
@@ -24,17 +28,19 @@ public class ActivityBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_layout);
 
+        patternHandler = new Handler();
+
         final LinearLayout gameBoard = (LinearLayout) findViewById(R.id.GameBoard);
 
-        final TextView patternString = (TextView) findViewById(R.id.PatternString);
+        final TextView patternString = (TextView) findViewById(R.id.pattern_textview);
         patternString.setText(PatternString);
+
 
         // Do stuff inside layout when pressed
         // Note the use of OnTouchListener instead of OnClick for the
         // Responsive behaviour we're going to be looking for
         LinearLayout greenButton = (LinearLayout) findViewById(R.id.green_button);
-        greenButton.setOnTouchListener(new View.OnTouchListener()
-        {
+        greenButton.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -59,7 +65,7 @@ public class ActivityBoard extends AppCompatActivity {
                     Log.d("Released", "Button released");
                     //Return background black on-released
                     gameBoard.setBackgroundColor(Color.parseColor("#000000"));
-                    
+
                 } else return false;
 
 
@@ -68,6 +74,54 @@ public class ActivityBoard extends AppCompatActivity {
             }
         });
 
+        final TextView startButton = (TextView) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Hide button once it's been pressed
+                startButton.setVisibility(View.GONE);
+
+                //Display pattern to user
+                startPattern();
+
+            }
+        });
     }
+
+
+    public void startPattern() {
+        new Thread(new Task()).start();
+    }
+
+    class Task implements Runnable {
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i < PatternString.length(); i++){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                patternHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // get digit at currentIndex and display 
+                        char currentDigit = PatternString.charAt(currentIndex);
+                        currentIndex = currentIndex + 1;
+
+                        TextView currentSquare = (TextView) findViewById(R.id.current_textview);
+                        currentSquare.setText(String.valueOf(currentDigit));
+
+
+
+                    }
+                });
+            }
+        }
+    }
+
 }
 
